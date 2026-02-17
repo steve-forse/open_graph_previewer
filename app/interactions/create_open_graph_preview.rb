160746e01815f2f3cb50b15378a1373a0ede4e5f
Steve Forse <steve.forse@gmail.com>
2026-02-17 20:43:43 +0700

    Broadcast OGP update on save

--- app/interactions/create_open_graph_preview.rb
+++ app/interactions/create_open_graph_preview.rb
@@ -1,10 +1,13 @@
 class CreateOpenGraphPreview < ApplicationInteraction
+  include BroadcastablePreview
+
   string :url
 
   def execute
     preview = OpenGraphPreview.new(url: url, status: :pending)
 
     if preview.save
+      broadcast_preview_update(preview)
       preview
     else
       preview.errors.each do |error|
