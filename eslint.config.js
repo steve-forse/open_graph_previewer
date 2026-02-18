30c0b6c5cf19c1460f98407d9a056bd39192450c
Steve Forse <steve.forse@gmail.com>
2026-02-18 20:15:23 +0700

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default tseslint.config(
+  { ignores: ["node_modules", "public", "tmp"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["app/frontend/**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
